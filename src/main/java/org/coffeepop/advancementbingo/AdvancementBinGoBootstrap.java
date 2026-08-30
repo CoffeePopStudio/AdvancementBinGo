cc58313f365827ddcda13bd0d7707ea406c082d4
oneachina <ruiruixi@hotmail.com>
2026-08-30 19:06:02 +0800

    chore: add plugin bootstrap class

---
+++ src/main/java/org/coffeepop/advancementbingo/AdvancementBinGoBootstrap.java
@@ -0,0 +1,12 @@
+package org.coffeepop.advancementbingo;
+
+import io.papermc.paper.plugin.bootstrap.BootstrapContext;
+import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
+
+class AdvancementBinGoBootstrap implements PluginBootstrap {
+
+    @Override
+    public void bootstrap(final BootstrapContext context) {
+        // Plugin bootstrap logic
+    }
+}
